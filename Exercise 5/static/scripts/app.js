3cfdedc590a101fa2a15309a1e9f79a31ee1e55b
Tijs Luitse - Imac <tijs.luitse@gmail.com>
2016-02-05 12:16:29 +0100

    Comments

--- Exercise 5/static/scripts/app.js
+++ Exercise 5/static/scripts/app.js
@@ -13,15 +13,16 @@
 		
 		init: function(){
 			
-			var currentHash = window.location.hash || '#landing';
-			document.querySelector(currentHash).classList.add('active');
+			var currentHash = window.location.hash || '#landing'; //als ie geen hash heeft krijgt ie landing als waarde en anders pakt hij die hash
+
+			document.querySelector(currentHash).classList.add('active'); //id wat actief is krijgt de class active
 
 			window.addEventListener("hashchange", function(){
 				
-				var newHash = window.location.hash;
+				var newHash = window.location.hash; //krijgt id van de nieuwe hash/url
 
-				sections.toggle(currentHash,newHash);
-				currentHash = newHash;
+				sections.toggle(currentHash, newHash); //geeft variabelen mee
+				currentHash = newHash; //omdat er is getoggled wordt de variabele upgedate
 
 			});
 		}
@@ -30,10 +31,10 @@
 
 	var sections = {
 
-		toggle: function(currentHash,newHash){
+		toggle: function(currentHash, newHash){
 
-			document.querySelector(currentHash).classList.remove('active');
-			document.querySelector(newHash).classList.add('active');
+			document.querySelector(currentHash).classList.remove('active'); 
+			document.querySelector(newHash).classList.add('active'); //nieuwe hash/id krijgt class name active
 			
 		}
 
